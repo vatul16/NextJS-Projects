03250347e6c5620d3c6b52ce5f1d5be874ad2263
vatul16 <vishwa.atul16@gmail.com>
2024-04-25 13:45:53 +0530

    Defining Parent Fetch in Child

--- discuss/src/components/posts/post-list.tsx
+++ discuss/src/components/posts/post-list.tsx
@@ -1,14 +1,19 @@
-import type { Post, User, Topic } from '@prisma/client';
-import Link from 'next/link';
-import paths from '@/paths';
+import type { PostWithData } from "@/db/queries/posts";
+import Link from "next/link";
+import paths from "@/paths";
 
-// TODO: Get list of posts into this component somehow
-export default function PostList() {
+interface PostListProps {
+  fetchData: () => Promise<PostWithData[]>;
+}
+
+export default async function PostList({ fetchData }: PostListProps) {
+  const posts = await fetchData();
+
   const renderedPosts = posts.map((post) => {
     const topicSlug = post.topic.slug;
 
     if (!topicSlug) {
-      throw new Error('Need a slug to link to a post');
+      throw new Error("Need a slug to link to a post");
     }
 
     return (
@@ -16,7 +21,7 @@ export default function PostList() {
         <Link href={paths.postShow(topicSlug, post.id)}>
           <h3 className="text-lg font-bold">{post.title}</h3>
           <div className="flex flex-row gap-8">
-            <p className="text-xs text-gray-400">By {post.user.name}</p>
+            <p className="text-xs text-gray-400">By {post.user.name}</p> 
             <p className="text-xs text-gray-400">
               {post._count.comments} comments
             </p>
