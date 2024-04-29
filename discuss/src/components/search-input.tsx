2cbd285976c5e8cbde832ec8e4a202ca5e852caa
vatul16 <tech@shypbuddy.com>
2024-04-29 11:11:56 +0530

    Redirecting from a Server Action

--- discuss/src/components/search-input.tsx
+++ discuss/src/components/search-input.tsx
@@ -2,9 +2,14 @@
 
 import { Input } from "@nextui-org/react";
 import { useSearchParams } from "next/navigation";
+import * as actions from "@/actions";
 
 export default function SearchInput() {
   const searchParams = useSearchParams();
 
-  return <Input defaultValue={searchParams.get("term") || ""} />; 
+  return (
+    <form action={actions.search}>
+        <Input name="term" defaultValue={searchParams.get("term") || ""} />
+    </form>
+  ); 
 }
