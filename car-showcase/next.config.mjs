950c6ddb41ba1734e2fe31350ef25980662a4308
vatul16 <tech@shypbuddy.com>
2024-04-29 12:56:57 +0530

    car showcase

---
+++ car-showcase/next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
+
+export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     reactStrictMode: true,
+//     output: "export",
+//     images: {
+//       unoptimized: true,
+//     },
+//   };
+
+//   export default nextConfig;
